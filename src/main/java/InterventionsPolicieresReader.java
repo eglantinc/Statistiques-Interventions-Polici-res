3425d6b50d73f4aaf9cf8e0c775b4ee01b96a571
Eglantine Clervil <clervil.eglantine_anne@courrier.uqam.ca>
2023-12-05 09:32:44 -0500

    Correction de bogue en tête fichier et création de tests fonctionnels

--- src/main/java/InterventionsPolicieresReader.java
+++ src/main/java/InterventionsPolicieresReader.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Cette classe permet de lire des données d'un fichier CSV d'interventions policières dont on a le chemin.
@@ -51,33 +52,27 @@ public class InterventionsPolicieresReader {
         // Récupérer uniquement le nom du fichier
         String nomFichier = Paths.get(cheminFichier).getFileName().toString();
 
-
         GestionChampsVides gestionnaireChampsVide = new GestionChampsVides(cheminFichier, PREMIERE_LIGNE);
 
         ArrayList<InterventionPoliciere> interventions = new ArrayList<>();
+
         try( BufferedReader lecteur = new BufferedReader(new FileReader(cheminFichier ) ) ) {
 
             String ligne;
 
             while( ( ligne = lecteur.readLine()) != null ) {
 
+                gererEnTeteFichierCsv(ligne);
+
                 if( numeroDeLigne > 1 ) {
 
                     String[] champs = ligne.split("," );
 
-                    // Tester si la longueur de la chaine est égale a 5
-                    GestionChampsVides.compterChampsAttendus(champs,numeroDeLigne);
-                    gestionnaireChampsVide.gererChampVide(champs,numeroDeLigne);
+                    gererLesChampsVidesFichierEntree(champs, gestionnaireChampsVide);
 
-                    InterventionPoliciere unObjetInterventionPoliciere = InterventionPoliciere
-                            .creerInstanceInterventionPoliciere (champs[0], champs[1], champs[2],
-                                    champs[3], champs[4]);
+                    InterventionPoliciere unObjetInterventionPoliciere = creerObjetInterventionPoliciere(champs, interventions);
 
-                    interventions.add( unObjetInterventionPoliciere );
-                    GestionDateIntervention.gererErreurDate(nomFichier, numeroDeLigne, unObjetInterventionPoliciere
-                            .getDate());
-                    GestionHeureIntervention.gererErreurFormatHeure (nomFichier, numeroDeLigne, unObjetInterventionPoliciere
-                            .getHeure());
+                    gererLesErreursTemporelles(nomFichier, unObjetInterventionPoliciere);
                 }
                 numeroDeLigne++;
             }
@@ -85,6 +80,40 @@ public class InterventionsPolicieresReader {
             throw new RuntimeException(TraducteurSingleton.getInstance().traduire("erreurLectureFichier", cheminFichier) );
         }
         return interventions;
+    }
+
+
+
+    private static void gererEnTeteFichierCsv(String ligne) {
+        // TODO: Refactor pour mieux gérer
+        String[] premiereLigneLu = ligne.split("," );
+        if (premiereLigneLu.length < 5) {
+            throw new RuntimeException("Invalide");
+        } else if (!(Arrays.equals(premiereLigneLu, PREMIERE_LIGNE))) {
+            throw new RuntimeException("Invalide");
+        }
+    }
+
+    private static void gererLesErreursTemporelles(String nomFichier, InterventionPoliciere unObjetInterventionPoliciere) {
+        GestionDateIntervention.gererErreurDate(nomFichier, numeroDeLigne, unObjetInterventionPoliciere
+                .getDate());
+        GestionHeureIntervention.gererErreurFormatHeure (nomFichier, numeroDeLigne, unObjetInterventionPoliciere
+                .getHeure());
+    }
+
+    private static void gererLesChampsVidesFichierEntree(String[] champs, GestionChampsVides gestionnaireChampsVide) {
+        // Tester si la longueur de la chaine est égale a 5
+        GestionChampsVides.compterChampsAttendus(champs,numeroDeLigne);
+        gestionnaireChampsVide.gererChampVide(champs,numeroDeLigne);
+    }
+
+    private static InterventionPoliciere creerObjetInterventionPoliciere(String[] champs, ArrayList<InterventionPoliciere> interventions) {
+        InterventionPoliciere unObjetInterventionPoliciere = InterventionPoliciere
+                .creerInstanceInterventionPoliciere (champs[0], champs[1], champs[2],
+                        champs[3], champs[4]);
+
+        interventions.add( unObjetInterventionPoliciere );
+        return unObjetInterventionPoliciere;
     }
 
 
