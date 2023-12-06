25474438f7c41cec7e656ad7a935b56954a65883
Eglantine Clervil <clervil.eglantine_anne@courrier.uqam.ca>
2023-12-05 20:39:46 -0500

    Refactoring: Réduire la classe InterventionsPolicieresWriter.java

--- src/main/java/InterventionsPolicieresWriter.java
+++ src/main/java/InterventionsPolicieresWriter.java
@@ -7,32 +7,20 @@ public class InterventionsPolicieresWriter {
     /**
      * Cette méthode sert à produire un fichier de sortie où qu'il y aura une nouvelle statistique sur le nombre de
      * parcs par arrondissements où il y a eu des interventions policières.
-     * 
+     *
      * @param fichierSortie Le chemin du fichier de sortie dans lequel on va produire la nouvelle statistique.
      * @param ligneSaisie La ligne de texte à écrire dans le fichier de sortie.
      */
-    public static void ecrireFichierSortie( String fichierSortie, String ligneSaisie ) {
+    public static void ecrireFichierSortie(String fichierSortie, String ligneSaisie) {
+        try (FileWriter fichierEcriture = new FileWriter(fichierSortie, true);
+             BufferedWriter tamponEcriture = new BufferedWriter(fichierEcriture)) {
 
-        try {
-
-            FileWriter fichierEcriture = new FileWriter(fichierSortie, true );
-
-            BufferedWriter tamponEcriture = new BufferedWriter(fichierEcriture);
-            // Écrire une ligne de chaines de caractères.
             tamponEcriture.write(ligneSaisie);
-
             tamponEcriture.newLine();
 
-            tamponEcriture.close();
-
-            fichierEcriture.close();
-
-        } catch( IOException e ) {
-
+        } catch (IOException e) {
             throw new RuntimeException(TraducteurSingleton.getInstance()
-                    .traduire("erreurEcritureDansFichier", fichierSortie));
-
+                    .traduire("erreurEcritureDansFichier", fichierSortie), e);
         }
     }
-
 }
