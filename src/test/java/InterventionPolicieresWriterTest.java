1bb2e866ed36a1e7d6938f7d73c19625b97bdd54
cj291957 <cj291957@ens.uqam.ca>
2023-12-07 13:28:59 -0500

    Début d'un mock object

---
+++ src/test/java/InterventionPolicieresWriterTest.java
@@ -0,0 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+//public class InterventionPolicieresWriterTest {
+//    public void ecrireFichierSortieTest(String fichierSortie, String ligneSaisie) {
+//        FileWriter fileWriterMock = mock(FileWriter.class);
+//        when(fileWriterMock(anyString(), true)).thenReturn()
+//        BufferedWriter bufferedWriterMock = mock(BufferedWriter.class);
+//
+//        bufferedWriterMock.write(ligneSaisie);
+//        bufferedWriterMock.newLine();
+//    }
+//}
