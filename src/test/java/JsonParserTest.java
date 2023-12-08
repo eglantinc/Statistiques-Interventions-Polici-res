1b2b6683952f6919077136ce71a9a596cfe898cd
Eglantine Clervil <clervil.eglantine_anne@courrier.uqam.ca>
2023-12-08 04:17:13 -0500

    Ajout et correction de tests

--- src/test/java/JsonParserTest.java
+++ src/test/java/JsonParserTest.java
@@ -15,5 +15,19 @@ public class JsonParserTest {
         traducteur.setLangueEnCours(Traducteur.FRANCAIS);
     }
 
+    @Test
+    public void chargerInfosFichierJsonDansArrayListTest() {
+        // Fichier quelconque et nom de l'array quelconque.
+        assertThrows(RuntimeException.class,
+                () -> JsonParser.chargerInfosFichierJsonArrayList("fichiernonexistant.json", "arraynonexistant"));
+    }
 
+
+    @Test
+    public void chargerInfosFichierJsonDansArrayListTest2() {
+        // Faute de frappe dans le nom de l'array json.
+        assertThrows(RuntimeException.class,
+                () -> JsonParser.chargerInfosFichierJsonArrayList
+                        ("arrondissements.json", "arrondissement"));
+    }
 }
