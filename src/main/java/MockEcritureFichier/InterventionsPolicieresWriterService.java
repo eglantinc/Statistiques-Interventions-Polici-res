39d26c0ac46f49f0ced18c6370bb9839652fd59e
Eglantine Clervil <clervil.eglantine_anne@courrier.uqam.ca>
2023-12-08 23:23:56 -0500

    Ajout de javadocs

--- src/main/java/MockEcritureFichier/InterventionsPolicieresWriterService.java
+++ src/main/java/MockEcritureFichier/InterventionsPolicieresWriterService.java
@@ -1,7 +1,26 @@
+/**
+ * L'interface {@code InterventionsPolicieresWriterService} définit la méthode pour écrire dans un fichier de sortie.
+ * Cette interface doit être implémentée par des services qui fournissent des fonctionnalités
+ * d'écriture dans un fichier pour les interventions policières
+ *
+ * @Author: Eglantine Clervil
+ * @Courriel: clervil.eglantine_anne@courrier.uqam.ca
+ * @Code-permanent: CLEE89530109
+ * @Groupe: 30
+ *
+ */
 package MockEcritureFichier;
 
 import java.io.IOException;
 
 public interface InterventionsPolicieresWriterService {
+
+    /**
+     * Écrit le contenu spécifié dans le fichier de sortie avec le nom spécifié.
+     *
+     * @param nomFichier le nom du fichier de sortie dans lequel écrire
+     * @param contenu le contenu à écrire dans le fichier
+     * @throws IOException si une erreur d'entrée/sortie survient lors de l'écriture dans le fichier
+     */
     void ecrireDansFichierSortie(String nomFichier, String contenu) throws IOException;
 }
--- src/main/java/MockEcritureFichier/MockInterventionsPolicieresWriter.java
+++ src/main/java/MockEcritureFichier/MockInterventionsPolicieresWriter.java
@@ -1,13 +1,37 @@
 package MockEcritureFichier;
 
+/**
+ * La classe MockInterventionsPolicieresWriter est une implémentation de
+ * InterventionsPolicieresWriterService utilisée à des fins de test. Elle enregistre
+ * les paramètres passés à la méthode ecrireDansFichierSortie pour permettre la vérification
+ * des appels lors des tests unitaires.
+ *
+ * @Author: Eglantine Clervil
+ * @Courriel: clervil.eglantine_anne@courrier.uqam.ca
+ * @Code-permanent: CLEE89530109
+ * @Groupe: 30
+ */
 public class MockInterventionsPolicieresWriter implements InterventionsPolicieresWriterService {
+
+    /**
+     * Le nom du fichier de sortie enregistré lors de l'appel à ecrireDansFichierSortie.
+     */
     public String fichierSortie;
+
+    /**
+     * La ligne de saisie enregistrée lors de l'appel à ecrireDansFichierSortie.
+     */
     public String ligneSaisie;
 
+    /**
+     * Enregistre le nom du fichier de sortie et la ligne de saisie lors de l'appel à cette méthode.
+     *
+     * @param nomFichier Le nom du fichier de sortie dans lequel écrire.
+     * @param contenu    Le contenu à écrire dans le fichier.
+     */
     @Override
     public void ecrireDansFichierSortie(String nomFichier, String contenu) {
         this.fichierSortie = nomFichier;
         this.ligneSaisie = contenu;
     }
-    }
-
+}
